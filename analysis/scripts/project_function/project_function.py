843c0be9a0776b5f54ce40c8ff51f109c827dc8a
Wendelin Giesler <wgiesler@mail.ubc.ca>
2020-11-15 18:48:24 +0100

    add py file

---
+++ analysis/scripts/project_function/project_function.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+from functools import reduce
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import pandas_profiling
+import matplotlib.dates as mdates
+
+# df = pd.read_csv(r'C:\Users\Len\Documents\GitHub\solo_502\course-project-solo_502\data\raw\ClimateData_Whistler.csv')
+
+
+def load_and_process(path_to_csv_file):
+    
+    # separating monthaly variables from the dataframe
+    # load data and select variables of interest
+    dataframe = (
+                pd.read_csv(path_to_csv_file)
+                .iloc[:,83:251]
+    )
+    
+    # Create dataframe with a column of labelled years
+    annual_m = np.arange(1901, 2020, 1).repeat(12)
+    annual_m2 = np.vstack(annual_m)
+    years_m = pd.DataFrame(data = annual_m2)
+    
+    # Create list of Strings for each month for the 119 years in the dataframe
+    months = [('Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')*119]
+    months2 = (
+        pd.DataFrame(months)
+        .melt()
+    )
+    
+
+    # Transpose and then melt monthly observations in Dataframe to get individual variables in separate columns and the months as observations instead of variables
+    global monthly_data
+    monthly_data = [months2, years_m] 
+    for i in range(14):
+        monthly_data.append(dataframe.iloc[:,i*12:(i+1)*12].T.melt())
+    
+    # Cleaning dataframe
+    monthly_data = (
+                    pd.concat(monthly_data, axis = 1)
+                    .drop(columns = 'variable')
+    )
+    
+    # Label selected columns of interest(variables)
+    monthly_data.columns = ['Month','Year','Max Temp', 'Min Temp', 'Ave Temp', 'Precipitation', 'Radiation', 'DDays < 0°C', 'DDays > 5°C', 
+                            'DDays < 18°C', 'DDays > 18°C', 'Frost Free Days', 'Snowfall', 'Evaporation', 
+                            'Moisture Deficit', 'Relative Humidity']
+#     print(monthly_data)
+
+
+    # separating seasonal winter variables from the dataframe for a better analysis 
+    dataframe_s = (
+                pd.read_csv(path_to_csv_file)
+                .iloc[:,[27,31,35,39,47,51,55,59,63,67,71,75,79]]
+    )
+    
+    # label years, fit together with rest of the dataframe
+    annual_w = np.arange(1901, 2020, 1)
+    annual_w2 = np.vstack(annual_w)
+    years_s = pd.DataFrame(data = annual_w2)
+    seasons = [years_s, dataframe_s]
+    
+    global seasonal_data
+    seasonal_data = pd.concat(seasons, axis = 1)
+    
+    # label columns (variables)
+    seasonal_data.columns = ['Year','Max Temp', 'Min Temp', 'Ave Temp', 'Precipitation', 'DDays < 0°C', 'DDays > 5°C', 
+                            'DDays < 18°C', 'DDays > 18°C', 'Frost Free Days', 'Snowfall', 'Evaporation', 
+                            'Moisture Deficit', 'Relative Humidity']
+#     print(seasonal_data)
+
+
+    # separating annual variables from the dataframe
+    dataframe_a = (
+                pd.read_csv(path_to_csv_file)
+                .iloc[:,4:27]
+    )
+    years = [years_s, dataframe_a]
+    
+    global annual_data
+    annual_data = pd.concat(years, axis = 1)
+    
+    # label columns (variables)
+    annual_data.columns = ['Year','Ave Temp','Warmest Month', 'Coldest Month','Difference', 'Mean Precip','May-Sep Precip','Heat-Moist I', 
+                           'Summer Heat-Moist I', 'DDays < 0°C', 'DDays > 5°C', 'DDays < 18°C', 'DDays > 18°C', 
+                           'Frost Free Days','Begin FFP','End FFP','Frost Free Period', 'Snowfall', 'Min Temp (30)', 'Max Temp (30)', 'Radiation',
+                           'Evaporation', 'Moisture Deficit', 'Relative Humidity']
+#     print(annual_data)
+    
+load_and_process(r'C:\Users\Len\Documents\GitHub\solo_502\course-project-solo_502\data\raw\ClimateData_Whistler.csv')
